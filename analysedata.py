035aa71b4b48d10579fd0a7d1623b7645fcf0578
m-triple-m <triplem656@gmail.com>
2021-05-31 14:05:54 +0530

    ..

--- analysedata.py
+++ analysedata.py
@@ -5,6 +5,11 @@ class Analyse:
     def __init__(self, path):
         self.df = pd.read_csv(path)
 
+    def getDataframe(self):
+        return self.df
 
     def getYearCount(self):
-        return self.df.groupby('year')['name'].count()
+        return self.df.groupby('Year')['Name'].count()
+
+    def getPlatformSum(self):
+        return self.df.groupby('Platform')['Global_Sales'].sum().sort_values(ascending=True)
--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 import streamlit as st
+from analysedata import Analyse
 
+
+def loadData():
+    print('loading..')
+    return Analyse('datasets/vgsales.csv')
+
+analysis = loadData()
 
 st.title("Video Game Sales Analysis")
 st.image('title_image.jpg')
-st.header("A Not very long description")
 
 sidebar = st.sidebar
 sidebar.header("Choose Your Option")
-choices = ["Select any option below", "View Dataset", "Analyse Timeline"]
+choices = ["Select any option below", "View Dataset", "Analyse Timeline", "Analyse Platform"]
 selOpt = sidebar.selectbox("Choose what to do", choices)
 
 def projectOverview():
     st.header('Project Overview')
+    st.header("A Not very long description")
+
     
+
 
 def viewDataset():
     st.header("Dataset Details")
@@ -20,6 +29,19 @@ def viewDataset():
 
 def analyseTimeline():
     st.header("Timeline analysis")
+    st.markdown('---')
+
+    col1, col2 = st.beta_columns(2)
+
+    col1.line_chart(analysis.getYearCount())
+    col2.bar_chart(analysis.getYearCount())
+
+
+def analysePlatform():
+    st.header("Video Games Platform analysis")
+    st.markdown('---')
+
+    st.bar_chart(analysis.getPlatformSum())
 
 
 if selOpt == choices[0]:
@@ -28,3 +50,5 @@ elif selOpt == choices[1]:
     viewDataset()
 elif selOpt == choices[2]:
     analyseTimeline()
+elif selOpt == choices[3]:
+    analysePlatform()
